3fe51e498a720b63c2a833998f53e43d4a52d84f
João Pedro Martins <mjoaopedro61@gmail.com>
2024-03-25 21:11:45 -0300

    :tada: That's going to be the fantastic NVIM config files

---
+++ plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    flavour = "latte",
+    background = { -- :h background
+      light = "latte",
+      dark = "mocha",
+    },
+    transparent_background = false,
+  }
+}
