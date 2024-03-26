4abbf78e7f1322d245565994c1e3d58c03f63488
João Pedro Martins <mjoaopedro61@gmail.com>
2024-03-25 21:46:42 -0300

    :lipstick: change base theme

--- colorscheme.lua
+++ colorscheme.lua
@@ -1,1 +1,1 @@
-return "catppuccin"
+return "blue"
--- plugins/catppuccin.lua
+++ plugins/catppuccin.lua
@@ -1,13 +1,5 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000,
-  opts = {
-    flavour = "latte",
-    background = { -- :h background
-      light = "latte",
-      dark = "mocha",
-    },
-    transparent_background = false,
-  }
+  priority = 1000
 }
