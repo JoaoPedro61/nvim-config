e2e652af7adb518b543ae01fcb9f9e91a994d666
João Pedro Martins <mjoaopedro61@gmail.com>
2024-05-01 23:53:50 -0300

    :sparkles: create a mason config with the default LSPs

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,29 @@
+return {
+  {
+  "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "tsserver",
+        "lua_ls",
+        "angularls",
+        "bashls",
+        "clangd",
+        "cssls",
+        "cssmodules_ls",
+        "css-variables-language-server",
+        "custom_elements_ls",
+        "dockerls",
+        "docker_compose_language_service",
+        "dotls",
+        "eslint",
+        "eslint_d",
+        "html",
+        "jsonls",
+        "luaformatter",
+        "prettier",
+        "rust_analyzer",
+        "tailwindcss",
+      },
+    },
+  }
+}
---
+++ lua/plugins/telescope-fzf-native.lua
@@ -0,0 +1,9 @@
+print("asçlfkasd");
+
+---@type LazySpec
+return {
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  },
+}
