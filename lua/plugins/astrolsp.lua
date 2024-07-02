867634cb2881cf59cb8600572abb1c3592af94a1
João Pedro Martins <mjoaopedro61@gmail.com>
2024-07-01 23:35:33 -0300

    :recycle: update mason-lspconfig to use astrolsp servers

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -5,142 +5,144 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrolsp",
-  ---@type AstroLSPOpts
-  opts = {
-    -- Configuration table of features provided by AstroLSP
-    features = {
-      autoformat = { enable = true }, -- enable or disable auto formatting on start
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = true, -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
-      icons_enabled = true, -- enable/disable icons for LSP diagnostics
-    },
-    -- customize lsp formatting options
-    formatting = {
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      -- Configuration table of features provided by AstroLSP
+      features = {
+        autoformat = { enable = true }, -- enable or disable auto formatting on start
+        codelens = true, -- enable/disable codelens refresh on start
+        inlay_hints = true, -- enable/disable inlay hints on start
+        semantic_tokens = true, -- enable/disable semantic token highlighting
+        icons_enabled = true, -- enable/disable icons for LSP diagnostics
+      },
+      -- customize lsp formatting options
+      formatting = {
+        -- control auto formatting on save
+        format_on_save = {
+          enabled = true, -- enable or disable format on save globally
+          allow_filetypes = { -- enable format on save for specified filetypes only
+            -- "go",
+          },
+          ignore_filetypes = { -- disable format on save for specified filetypes
+            -- "python",
+          },
         },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+        disabled = { -- disable formatting capabilities for the listed language servers
+          -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+          -- "lua_ls",
+        },
+        timeout_ms = 1000, -- default format timeout
+        -- filter = function(client) -- fully override the default formatting function
+        --   return true
+        -- end
+      },
+      -- enable servers that you already have installed without mason
+      servers = {
+        "tsserver",
+        "lua_ls",
+        "angularls",
+        "bashls",
+        "cssls",
+        "cssmodules_ls",
+        "dockerls",
+        "docker_compose_language_service",
+        "dotls",
+        "eslint",
+        "html",
+        "jsonls",
+        "tailwindcss"
+      },
+      -- customize language server configuration options passed to `lspconfig`
+      ---@diagnostic disable: missing-fields
+      config = {
+        -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = { enable = true, arrayIndex = "Disable" },
+            },
+          },
+        },
+        clangd = {
+          capabilities = {
+            offsetEncoding = "utf-8",
+          },
+        },
+        tsserver = {
+          root_dir = require("lspconfig.util").root_pattern("package.json"),
         },
       },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+      -- customize how language servers are attached
+      handlers = {
+        -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
+        -- function(server, opts) require("lspconfig")[server].setup(opts) end
+
+        -- the key is the server that is being setup with `lspconfig`
+        -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
+        -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
       },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
-    },
-    -- enable servers that you already have installed without mason
-    servers = {
-      "tsserver",
-      "lua_ls",
-      "angularls",
-      "bashls",
-      "cssls",
-      "cssmodules_ls",
-      "dockerls",
-      "docker_compose_language_service",
-      "dotls",
-      "eslint",
-      "html",
-      "jsonls",
-      "tailwindcss"
-    },
-    -- customize language server configuration options passed to `lspconfig`
-    ---@diagnostic disable: missing-fields
-    config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-      lua_ls = {
-        settings = {
-          Lua = {
-            hint = { enable = true, arrayIndex = "Disable" },
+      -- Configure buffer local auto commands to add when attaching a language server
+      autocmds = {
+        -- first key is the `augroup` to add the auto commands to (:h augroup)
+        lsp_document_highlight = {
+          -- Optional condition to create/delete auto command group
+          -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
+          -- condition will be resolved for each client on each execution and if it ever fails for all clients,
+          -- the auto commands will be deleted for that buffer
+          cond = "textDocument/documentHighlight",
+          -- cond = function(client, bufnr) return client.name == "lua_ls" end,
+          -- list of auto commands to set
+          {
+            -- events to trigger
+            event = { "CursorHold", "CursorHoldI" },
+            -- the rest of the autocmd options (:h nvim_create_autocmd)
+            desc = "Document Highlighting",
+            callback = function() vim.lsp.buf.document_highlight() end,
+          },
+          {
+            event = { "CursorMoved", "CursorMovedI", "BufLeave" },
+            desc = "Document Highlighting Clear",
+            callback = function() vim.lsp.buf.clear_references() end,
           },
         },
       },
-      clangd = {
-        capabilities = {
-          offsetEncoding = "utf-8",
+      -- Configure buffer local user commands to add when attaching a language server
+      commands = {
+        Format = {
+          function() vim.lsp.buf.format() end,
+          -- condition to create the user command
+          -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
+          cond = "textDocument/formatting",
+          -- the rest of the user command options (:h nvim_create_user_command)
+          desc = "Format file with LSP",
         },
       },
-      tsserver = {
-        root_dir = require("lspconfig.util").root_pattern("package.json"),
-      },
-    },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
-
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    },
-    -- Configure buffer local auto commands to add when attaching a language server
-    autocmds = {
-      -- first key is the `augroup` to add the auto commands to (:h augroup)
-      lsp_document_highlight = {
-        -- Optional condition to create/delete auto command group
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-        -- the auto commands will be deleted for that buffer
-        cond = "textDocument/documentHighlight",
-        -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-        -- list of auto commands to set
-        {
-          -- events to trigger
-          event = { "CursorHold", "CursorHoldI" },
-          -- the rest of the autocmd options (:h nvim_create_autocmd)
-          desc = "Document Highlighting",
-          callback = function() vim.lsp.buf.document_highlight() end,
-        },
-        {
-          event = { "CursorMoved", "CursorMovedI", "BufLeave" },
-          desc = "Document Highlighting Clear",
-          callback = function() vim.lsp.buf.clear_references() end,
+      -- mappings to be set up on attaching of a language server
+      mappings = {
+        n = {
+          gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
+          -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
+          -- gD = {
+          --   function() vim.lsp.buf.declaration() end,
+          --   desc = "Declaration of current symbol",
+          --   cond = "textDocument/declaration",
+          -- },
+          -- ["<Leader>uY"] = {
+          --   function() require("astrolsp.toggles").buffer_semantic_tokens() end,
+          --   desc = "Toggle LSP semantic highlight (buffer)",
+          --   cond = function(client) return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens end,
+          -- },
         },
       },
+      -- A custom `on_attach` function to be run after the default `on_attach` function
+      -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
+      on_attach = function(client, bufnr)
+        -- this would disable semanticTokensProvider for all clients
+        -- client.server_capabilities.semanticTokensProvider = nil
+      end,
     },
-    -- Configure buffer local user commands to add when attaching a language server
-    commands = {
-      Format = {
-        function() vim.lsp.buf.format() end,
-        -- condition to create the user command
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        cond = "textDocument/formatting",
-        -- the rest of the user command options (:h nvim_create_user_command)
-        desc = "Format file with LSP",
-      },
-    },
-    -- mappings to be set up on attaching of a language server
-    mappings = {
-      n = {
-        gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
-        -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        -- gD = {
-        --   function() vim.lsp.buf.declaration() end,
-        --   desc = "Declaration of current symbol",
-        --   cond = "textDocument/declaration",
-        -- },
-        -- ["<Leader>uY"] = {
-        --   function() require("astrolsp.toggles").buffer_semantic_tokens() end,
-        --   desc = "Toggle LSP semantic highlight (buffer)",
-        --   cond = function(client) return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens end,
-        -- },
-      },
-    },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    on_attach = function(client, bufnr)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
-    end,
   },
 
   -- set up mason-lspconfig for AstroLSP
@@ -154,7 +156,12 @@ return {
         opts = function()
           return {
             -- use AstroLSP setup for mason-lspconfig
-            handlers = { function(server) require("astrolsp").lsp_setup(server) end },
+            handlers = {
+              function(server)
+                require("astrolsp").lsp_setup(server)
+              end
+            },
+            ensure_installed = require("astrolsp").config.servers,
           }
         end,
       },
